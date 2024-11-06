40a5b08829c7af8c868e7a5c5659b47762801f49
Mac <mac@MacBook-Pro-Mac.local>
2024-11-06 16:55:59 +0300

    update

--- web/app/themes/sage/resources/views/components/products/grid.blade.php
+++ web/app/themes/sage/resources/views/components/products/grid.blade.php
@@ -13,74 +13,91 @@
     </div>
 
     <div class="product-card">
-        <h3 class="product-card__title">Компрессорные станции (КС)</h3>
-        <div class="product-card__image-container">
-            <img src="@asset('images/products/product2.png')" alt="Компрессорные станции" class="product-card__image">
-        </div>
-        <a href="#" class="product-card__circle-button">
-            <span class="product-card__circle-icon">→</span>
+        <a href="/product/компрессорные-станции-кс">
+            <h3 class="product-card__title">Компрессорные станции (КС)</h3>
+            <div class="product-card__image-container">
+                <img src="@asset('images/products/product2.png')" alt="Компрессорные станции" class="product-card__image">
+            </div>
+            <a href="#" class="product-card__circle-button">
+                <span class="product-card__circle-icon">→</span>
+            </a>
         </a>
     </div>
 
     <div class="product-card">
-        <h3 class="product-card__title">Насосные станции (НС)</h3>
-        <div class="product-card__image-container">
-            <img src="@asset('images/products/product3.png')" alt="Насосные станции" class="product-card__image">
-        </div>
-        <a href="#" class="product-card__circle-button">
-            <span class="product-card__circle-icon">→</span>
+        <a href="/product/насосные-станции-нс/">
+            <h3 class="product-card__title">Насосные станции (НС)</h3>
+            <div class="product-card__image-container">
+                <img src="@asset('images/products/product3.png')" alt="Насосные станции" class="product-card__image">
+            </div>
+            <a href="#" class="product-card__circle-button">
+                <span class="product-card__circle-icon">→</span>
+            </a>
         </a>
     </div>
 
     <div class="product-card">
-        <h3 class="product-card__title">Блочные модульные здания и металлоконструкции</h3>
-        <div class="product-card__image-container">
-            <img src="@asset('images/products/product4.png')" alt="Блочные модульные здания и металлоконструкции"
-                class="product-card__image">
-        </div>
-        <a href="#" class="product-card__circle-button">
-            <span class="product-card__circle-icon">→</span>
+        <a href="/product/блочные-модульные-здания-и-металлоко/">
+            <h3 class="product-card__title">Блочные модульные здания и металлоконструкции</h3>
+            <div class="product-card__image-container">
+                <img src="@asset('images/products/product4.png')" alt="Блочные модульные здания и металлоконструкции"
+                    class="product-card__image">
+            </div>
+            <a href="#" class="product-card__circle-button">
+                <span class="product-card__circle-icon">→</span>
+            </a>
+        </a>
+    </div>
+
+
+    <div class="product-card">
+        <a href="/product/автоматизированные-системы-управлен/">
+            <h3 class="product-card__title">Автоматизированные системы управления (АСУ)</h3>
+            <div class="product-card__image-container">
+                <img src="@asset('images/products/product5.png')" alt="Автоматизированные системы управления" class="product-card__image">
+            </div>
+            <a href="#" class="product-card__circle-button">
+                <span class="product-card__circle-icon">→</span>
+            </a>
         </a>
     </div>
 
     <div class="product-card">
-        <h3 class="product-card__title">Автоматизированные системы управления (АСУ)</h3>
-        <div class="product-card__image-container">
-            <img src="@asset('images/products/product5.png')" alt="Автоматизированные системы управления" class="product-card__image">
-        </div>
-        <a href="#" class="product-card__circle-button">
-            <span class="product-card__circle-icon">→</span>
-        </a>
-    </div>
-
-    <div class="product-card">
-        <h3 class="product-card__title">Установки дозирования химических реагентов (УДХР)</h3>
-        <div class="product-card__image-container">
-            <img src="@asset('images/products/product6.png')" alt="Установки дозирования химических реагентов" class="product-card__image">
-        </div>
-        <a href="#" class="product-card__circle-button">
-            <span class="product-card__circle-icon">→</span>
+        <a href="/product/установки-дозирования-химических-ре/">
+            <h3 class="product-card__title">Установки дозирования химических реагентов (УДХР)</h3>
+            <div class="product-card__image-container">
+                <img src="@asset('images/products/product6.png')" alt="Установки дозирования химических реагентов"
+                    class="product-card__image">
+            </div>
+            <a href="#" class="product-card__circle-button">
+                <span class="product-card__circle-icon">→</span>
+            </a>
         </a>
     </div>
 
     <div class="product-card product-card--wide">
-        <h3 class="product-card__title">Поставка запчастей к нефтепромысловому оборудованию</h3>
-        <div class="product-card__image-container">
-            <img src="@asset('images/products/product7.png')" alt="Поставка запчастей к нефтепромысловому оборудованию"
-                class="product-card__image_wide">
-        </div>
-        <a href="#" class="product-card__circle-button">
-            <span class="product-card__circle-icon">→</span>
+        <a href="/product/поставка-запчастей-к-нефтепромыслов/">
+            <h3 class="product-card__title">Поставка запчастей к нефтепромысловому оборудованию</h3>
+            <div class="product-card__image-container">
+                <img src="@asset('images/products/product7.png')" alt="Поставка запчастей к нефтепромысловому оборудованию"
+                    class="product-card__image_wide">
+            </div>
+            <a href="#" class="product-card__circle-button">
+                <span class="product-card__circle-icon">→</span>
+            </a>
         </a>
     </div>
 
     <div class="product-card">
-        <h3 class="product-card__title">Нестандартная продукция</h3>
-        <div class="product-card__image-container">
-            <img src="@asset('images/products/product8.png')" alt="Нестандартная продукция" class="product-card__image">
-        </div>
-        <a href="#" class="product-card__circle-button">
-            <span class="product-card__circle-icon">→</span>
+        <a href="/product/нестандартная-продукция/">
+            <h3 class="product-card__title">Нестандартная продукция</h3>
+            <div class="product-card__image-container">
+                <img src="@asset('images/products/product8.png')" alt="Нестандартная продукция" class="product-card__image">
+            </div>
+            <a href="#" class="product-card__circle-button">
+                <span class="product-card__circle-icon">→</span>
+            </a>
         </a>
     </div>
+
 </div>
--- web/app/themes/sage/resources/views/components/products/slider.blade.php
+++ web/app/themes/sage/resources/views/components/products/slider.blade.php
@@ -20,10 +20,14 @@
             </div>
             <div class="slide">
                 <div class="product-card" style="height: 260px">
-                    <h3 class="product-card__title">Компрессорные станции (КС)</h3>
-                    <div class="product-card__image-container">
-                        <img src="@asset('images/products/product2.png')" alt="Компрессорные станции" class="product-card__image">
-                    </div>
+                    <a href="/product/компрессорные-станции-кс/">
+                        <h3 class="product-card__title">Компрессорные станции (КС)</h3>
+                    </a>
+                    <a href="/product/компрессорные-станции-кс/">
+                        <div class="product-card__image-container">
+                            <img src="@asset('images/products/product2.png')" alt="Компрессорные станции" class="product-card__image">
+                        </div>
+                    </a>
                     <a href="#" class="product-card__circle-button">
                         <span class="product-card__circle-icon">→</span>
                     </a>
@@ -31,10 +35,14 @@
             </div>
             <div class="slide">
                 <div class="product-card" style="height: 260px">
-                    <h3 class="product-card__title">Насосные станции (НС)</h3>
-                    <div class="product-card__image-container">
-                        <img src="@asset('images/products/product3.png')" alt="Насосные станции" class="product-card__image">
-                    </div>
+                    <a href="product/насосные-станции-нс/">
+                        <h3 class="product-card__title">Насосные станции (НС)</h3>
+                    </a>
+                    <a href="product/насосные-станции-нс/">
+                        <div class="product-card__image-container">
+                            <img src="@asset('images/products/product3.png')" alt="Насосные станции" class="product-card__image">
+                        </div>
+                    </a>
                     <a href="#" class="product-card__circle-button">
                         <span class="product-card__circle-icon">→</span>
                     </a>
@@ -42,11 +50,14 @@
             </div>
             <div class="slide">
                 <div class="product-card" style="height: 260px">
-                    <h3 class="product-card__title">Блочные модульные здания и металлоконструкции</h3>
-                    <div class="product-card__image-container">
-                        <img src="@asset('images/products/product4.png')" alt="Блочные модульные здания и металлоконструкции"
-                            class="product-card__image">
-                    </div>
+                    <a href="product/насосные-станции-нс/">
+                        <h3 class="product-card__title">Насосные станции (НС)</h3>
+                    </a>
+                    <a href="product/насосные-станции-нс/">
+                        <div class="product-card__image-container">
+                            <img src="@asset('images/products/product3.png')" alt="Насосные станции" class="product-card__image">
+                        </div>
+                    </a>
                     <a href="#" class="product-card__circle-button">
                         <span class="product-card__circle-icon">→</span>
                     </a>
@@ -54,11 +65,15 @@
             </div>
             <div class="slide">
                 <div class="product-card" style="height: 260px">
-                    <h3 class="product-card__title">Автоматизированные системы управления (АСУ)</h3>
-                    <div class="product-card__image-container">
-                        <img src="@asset('images/products/product5.png')" alt="Автоматизированные системы управления"
-                            class="product-card__image">
-                    </div>
+                    <a href="product/автоматизированные-системы-управлен/">
+                        <h3 class="product-card__title">Автоматизированные системы управления (АСУ)</h3>
+                    </a>
+                    <a href="product/автоматизированные-системы-управлен/">
+                        <div class="product-card__image-container">
+                            <img src="@asset('images/products/product5.png')" alt="Автоматизированные системы управления"
+                                class="product-card__image">
+                        </div>
+                    </a>
                     <a href="#" class="product-card__circle-button">
                         <span class="product-card__circle-icon">→</span>
                     </a>
@@ -66,11 +81,15 @@
             </div>
             <div class="slide">
                 <div class="product-card" style="height: 260px">
-                    <h3 class="product-card__title">Установки дозирования химических реагентов (УДХР)</h3>
-                    <div class="product-card__image-container">
-                        <img src="@asset('images/products/product6.png')" alt="Установки дозирования химических реагентов"
-                            class="product-card__image">
-                    </div>
+                    <a href="/product/установки-дозирования-химических-ре/">
+                        <h3 class="product-card__title">Установки дозирования химических реагентов (УДХР)</h3>
+                    </a>
+                    <a href="/product/установки-дозирования-химических-ре/">
+                        <div class="product-card__image-container">
+                            <img src="@asset('images/products/product6.png')" alt="Установки дозирования химических реагентов"
+                                class="product-card__image">
+                        </div>
+                    </a>
                     <a href="#" class="product-card__circle-button">
                         <span class="product-card__circle-icon">→</span>
                     </a>
@@ -78,11 +97,15 @@
             </div>
             <div class="slide" style="height: 260px">
                 <div class="product-card product-card">
-                    <h3 class="product-card__title">Поставка запчастей к нефтепромысловому оборудованию</h3>
-                    <div class="product-card__image-container">
-                        <img src="@asset('images/products/product7.png')" alt="Поставка запчастей к нефтепромысловому оборудованию"
-                            class="product-card__image">
-                    </div>
+                    <a href="/product/поставка-запчастей-к-нефтепромыслов/">
+                        <h3 class="product-card__title">Поставка запчастей к нефтепромысловому оборудованию</h3>
+                    </a>
+                    <a href="/product/поставка-запчастей-к-нефтепромыслов/">
+                        <div class="product-card__image-container">
+                            <img src="@asset('images/products/product7.png')" alt="Поставка запчастей к нефтепромысловому оборудованию"
+                                class="product-card__image">
+                        </div>
+                    </a>
                     <a href="#" class="product-card__circle-button">
                         <span class="product-card__circle-icon">→</span>
                     </a>
@@ -90,10 +113,14 @@
             </div>
             <div class="slide">
                 <div class="product-card" style="height: 260px">
-                    <h3 class="product-card__title">Нестандартная продукция</h3>
-                    <div class="product-card__image-container">
-                        <img src="@asset('images/products/product8.png')" alt="Нестандартная продукция" class="product-card__image">
-                    </div>
+                    <a href="/product/нестандартная-продукция/">
+                        <h3 class="product-card__title">Нестандартная продукция</h3>
+                    </a>
+                    <a href="/product/нестандартная-продукция/">
+                        <div class="product-card__image-container">
+                            <img src="@asset('images/products/product8.png')" alt="Нестандартная продукция" class="product-card__image">
+                        </div>
+                    </a>
                     <a href="#" class="product-card__circle-button">
                         <span class="product-card__circle-icon">→</span>
                     </a>
